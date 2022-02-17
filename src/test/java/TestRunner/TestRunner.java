cfb0bf04410e4e9feee2725589ed1d4b4248cf5d
Akhouri <alisha.akhouri1@vodafone.com>
2022-02-17 17:56:59 +0530

    added cucumber.json

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Feature/",glue= {"StepDefination"},
 monochrome = true,
-plugin={"pretty","html:target/HtmlReports","json:target/JSONReport/report.json"},
+plugin={"pretty","html:target/HtmlReports","json:target/JSONReport/report.json","json:target/cuucmber.json"},
 		tags="@smoke"
 		)
 public class TestRunner {
