3f81420b5ea35b63016efb1786c56291d9f4321d
Akhouri <alisha.akhouri1@vodafone.com>
2022-02-17 22:17:41 +0530

    working cucumber reports

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -5,9 +5,10 @@ import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Feature/",glue= {"StepDefination"},
+@CucumberOptions(features="src/test/resources/Feature/",glue= {"StepDefination"}, 
 monochrome = true,
-plugin={"pretty","html:target/HtmlReports","json:target/JSONReport/report.json","json:target/cuucmber.json"},
+plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"},
+
 		tags="@smoke"
 		)
 public class TestRunner {
